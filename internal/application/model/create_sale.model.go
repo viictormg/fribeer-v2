cd6094fdbb3a36bf09620014ec1e38ac196cf53b
victor <victorfabrica2019@gmail.com>
2023-02-25 16:24:16 -0500

    add validation body request create sale

---
+++ internal/application/model/create_sale.model.go
@@ -0,0 +1,16 @@
+package model
+
+type CreateSaleModel struct {
+	CustomerID string                   `json:"customerID"`
+	State      string                   `json:"state"`
+	SaleDate   string                   `json:"saleDate"`
+	Comments   string                   `json:"comments"`
+	Products   []ProductCreateSaleModel `json:"products"`
+}
+
+type ProductCreateSaleModel struct {
+	ProductID   string  `json:"productID"`
+	InitialDate string  `json:"initialDate"`
+	EndDate     string  `json:"endDate"`
+	Quantity    float32 `json:"quantity"`
+}
--- internal/infrastructure/entrypoints/api/sale/create_sale.handler.go
+++ internal/infrastructure/entrypoints/api/sale/create_sale.handler.go
@@ -5,12 +5,25 @@ import (
 	"time"
 
 	"github.com/labstack/echo/v4"
+	"github.com/viictormg/fribeer-v2/internal/application/model"
 	"github.com/viictormg/fribeer-v2/internal/domain/constants"
 	"github.com/viictormg/fribeer-v2/internal/domain/dto"
 	infradto "github.com/viictormg/fribeer-v2/internal/infrastructure/entrypoints/api"
 )
 
 func (saleHandler *SaleHandler) CreateSaleHandler(c echo.Context) error {
+	var body model.CreateSaleModel
+
+	err := c.Bind(&body)
+
+	if err != nil {
+		response := infradto.Response{
+			Success:   false,
+			Error:     []string{constants.ErrorDecodeBody},
+			Timestamp: time.Now(),
+		}
+		return c.JSON(http.StatusBadRequest, response)
+	}
 
 	response := infradto.Response{
 		Success:   true,
